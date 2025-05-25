e03835fc49948e85b6ca6d7a978e3f1bbf201f04
jacebright <jacebright@gmail.com>
2025-05-24 21:18:00 -0500

    W03

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { Metadata } from 'next';
 
 export default function Page() {
   return (
@@ -48,3 +49,7 @@ export default function Page() {
     </main>
   );
 }
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
